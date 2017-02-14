8f48347b5560a5fd2a6bd4e423ab508e7eb8d3fc
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-02-14 17:55:50 +1100

    Changed variable names

--- automations/winchautomation.py
+++ automations/winchautomation.py
@@ -8,37 +8,37 @@ class AutonomousWinch(StateMachine):
     sd = NetworkTable
 
     @state(first=True)
-    def retractPiston(self):
+    def retract_piston(self):
         self.sd.putString("state", "climbing")
         self.winch.rope_lock_solenoid_reverse()
-        self.next_state("onMotor")
+        self.next_state("on_motor")
 
-    @timed_state(duration=2, must_finish=True, next_state="rotateWinch")
-    def onMotor(self):
+    @timed_state(duration=2, must_finish=True, next_state="rotate_winch")
+    def on_motor(self):
         self.winch.rotate_winch(0.3)
 
     @state(must_finish=True)
     def rotateWinch(self):
         if self.winch.on_rope_engaged():
-            self.next_state("firePiston")
+            self.next_state("fire_piston")
 
     @state(must_finish=True)
-    def firePiston(self):
+    def fire_piston(self):
         self.winch.rope_lock_solenoid_forward()
         self.winch.rotate_winch(0.8)    #set climbing speed
-        self.next_state("onMotorFull")
+        self.next_state("on_motor_full")
 
-    @timed_state(duration=2, must_finish=True, next_state="touchpadPressed")
-    def onMotorFull(self):
+    @timed_state(duration=2, must_finish=True, next_state="touchpad_pressed")
+    def on_motor_full(self):
         pass
 
     @state(must_finish=True)
-    def touchpadPressed(self):
+    def touchpad_pressed(self):
         if self.winch.on_touchpad_engaged():
-            self.next_state("stopMotor")
+            self.next_state("stop_motor")
 
     @state(must_finish=True)
-    def stopMotor(self):
+    def stop_motor(self):
         self.winch.rotate_winch(0)
         self.sd.putString("state", "stationary")
         self.done()
