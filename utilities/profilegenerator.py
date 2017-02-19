fecc5489d8380b2b0f41c7e6f52c22275198ef57
Arthur Allshire <arthur@thedropbears.org.au>
2017-02-19 21:25:17 +1100

    Get negative displacement trajectories going

--- utilities/profilegenerator.py
+++ utilities/profilegenerator.py
@@ -25,9 +25,14 @@ def generate_trapezoidal_trajectory(
 
     :returns: a list of (pos, vel acc) tuples
     """
+    direction = sign(x_final-x_start)
 
     # area under the velocity-time trapezoid
     x = x_final - x_start
+
+    v_max = abs(v_max)*direction
+    a_pos = abs(a_pos)*direction
+    a_neg = -abs(a_neg)*direction
 
     if x == 0:
         return [(x_start, v_start, 0.0)]
@@ -36,10 +41,8 @@ def generate_trapezoidal_trajectory(
     # and decelerating. Used as max velocity in cases where we don't hit the
     # robot's top speed
     triangular_max = math.sqrt(
-            (2*abs(x)*a_pos*a_neg+a_neg*v_start**2-a_pos*v_final**2)/(a_neg-a_pos))
+            (2*x*a_pos*a_neg+a_neg*v_start**2-a_pos*v_final**2)/(a_neg-a_pos))
     v_max = min(v_max, triangular_max)
-
-    direction = sign(x)
 
     # time (since the start of the trajectory) that we hit v_max
     t_cruise = (v_max - v_start)/a_pos
@@ -48,7 +51,7 @@ def generate_trapezoidal_trajectory(
     # time it takes to slow down to v_final
     t_slow = (v_final - v_max)/a_neg
     # time at which we start decelerating
-    t_decel = (abs(x)-t_cruise*(v_start + v_max)/2
+    t_decel = (x-t_cruise*(v_start + v_max)/2
             - t_slow*(v_final + v_max)/2)/v_max + t_cruise
     # how long we are cruising at v_max for (flat part of the trapezoid)
     t_constant = t_decel - t_cruise
@@ -65,25 +68,24 @@ def generate_trapezoidal_trajectory(
     segments = []
     for i in range(0, num_segments+1):
         # velocity in the current timestep
-        v = direction*((v_max-v_start)*i/num_segments+v_start)
+        v = (v_max-v_start)*i/num_segments+v_start
         segments.append((
-                direction*(((v+v_start)/2)*t_cruise*i/num_segments),
-                direction*v, direction*a_pos))
+                ((v+v_start)/2)*t_cruise*i/num_segments,
+                v, a_pos))
 
     # interpolate along the cruise section of the path
     # do it as a list comprehension so that it runs faster
     num_segments = int(t_decel*Chassis.motion_profile_speed - num_segments)
-    segments += [(direction
-        * (x_cruise + v_max * (t_decel-t_cruise) * i / num_segments),
-                  direction*v_max, 0) for i in range(1, num_segments+1)]
+    segments += [(
+        (x_cruise + v_max * (t_decel-t_cruise) * i / num_segments),
+                  v_max, 0) for i in range(1, num_segments+1)]
 
     # interpolate along the deceleration portion of the path
     num_segments = int((t_f-t_decel)*Chassis.motion_profile_speed)
     for i in range(1, num_segments+1):
         v = v_max - (v_max-v_final) * i/num_segments
         segments.append((
-            direction
-            * (x_decel + (v+v_max)/2 * (t_f-t_decel) * i/num_segments),
-            direction*v, direction*a_neg))
+            x_decel + (v+v_max)/2 * (t_f-t_decel) * i/num_segments,
+            v, a_neg))
 
     return segments
