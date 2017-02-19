6f8e6856cc1a286edd5b8dae12c740d9075876dc
Joel Bryla <joel@wiredlizard.com>
2017-02-19 13:08:30 +1100

    slowed reverse climb speed

--- robot.py
+++ robot.py
@@ -139,7 +139,7 @@ class Robot(magicbot.MagicRobot):
                 # backdrive the winch
                 self.winch_automation.done()
                 self.winch.change_control_mode(False)
-                self.winch_motor.set(-0.4)
+                self.winch_motor.set(-0.3)
         except:
             self.onException()
 
