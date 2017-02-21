1497323ab3c4b64f2c756c24557c0ba3cc3edc11
James Ward <j.ward@acfr.usyd.edu.au>
2017-02-21 11:57:52 +1100

    Use contours to find midpoint between targets, rather than centre of mass.

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -5,4 +5,4 @@ import vision
 
 def test_black():
     res = vision.find_target(np.zeros(shape=(320, 240, 3), dtype=np.uint8))
-    assert res[0] is None
+    assert res[2] is 0
--- vision.py
+++ vision.py
@@ -1,9 +1,8 @@
 import numpy as np
 import cv2
+import heapq
 
-
-MIN_MASKED_RATIO = 0.1
-
+AREA_THRESHOLD = 0.0005
 
 def loop():
     import cscore as cs
@@ -41,10 +40,8 @@ def loop():
             # We got an error; report it through the output stream.
             cvSource.notifyError(cvsink.getError())
         else:
-            x, img, masked_ratio = find_target(frame)
-            if masked_ratio > MIN_MASKED_RATIO:
-                loc = int((x+1) * width // 2)
-                cv2.line(img, (loc, 60), (loc, 180), (255, 255, 0), thickness=2, lineType=8, shift=0)
+            x, img, num_targets = find_target(frame)
+            if num_targets > 0:
                 nt.putNumber("x", x)
                 nt.putNumber("time", time)
             cvSource.putFrame(img)
@@ -66,14 +63,39 @@ def find_target(img, lower=np.array([110/2, 200, 75]), upper=np.array([155/2, 25
     res = cv2.bitwise_and(img, img, mask=mask)
 
     height, width = mask.shape
-    masked_pixels = mask.sum()
-    masked_ratio = masked_pixels / (height*width)
+    # Get the information for the contours
+    _, contours, __ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # sort the contours into a list
+    areas = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area/(height*width) > AREA_THRESHOLD:
+            heapq.heappush(areas, (cv2.contourArea(contour), contour))
 
-    if masked_ratio > MIN_MASKED_RATIO:
-        X, Y = np.meshgrid(range(0, width), range(0, height))
-        x_coord = int((X * mask).sum() / masked_pixels)
-    else:
-        return None, res, masked_ratio
-
+    areas = heapq.nlargest(2, areas)
+    x_coord = 0
+    for _, contour in areas:
+        moments = cv2.moments(contour)
+        x_coord += moments['m10']/moments['m00'] / len(areas)
+        cv2.drawContours(res, (contour, ), -1, (255, 0, 0), 1)
+    if len(areas) > 0:
+        cv2.line(res, (int(x_coord), 60), (int(x_coord), 180), (255,255,0), thickness=2, lineType=8, shift=0)
     pos = 2 * x_coord / width - 1
-    return pos, res, masked_ratio
+    return pos, res, len(areas)
+
+# Allow easy testing of captured sample images
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Test vision system from command line.')
+    parser.add_argument('--showfile', help='display a specific file with a bounding box drawn around it',
+            type=str, default=None)
+    args = parser.parse_args()
+    if args.showfile:
+        image = cv2.imread(args.showfile, cv2.IMREAD_COLOR)
+        x, image, num_targets = find_target(image)
+        cv2.imshow('image', image)
+    while cv2.getWindowHandle('image') >= 0:
+        if cv2.waitKey(50) != -1:
+            break
+
